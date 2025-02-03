81d6b6c67ec53885fb198c26e48cf6f81cf28cf7
Jeffrey Ching <jchingny@gmail.com>
2025-02-03 18:42:39 -0500

    Update lambdas_start.py

--- archive/Ex_Files_Adv_Python/Exercise Files/03 Functions/lambdas_start.py
+++ archive/Ex_Files_Adv_Python/Exercise Files/03 Functions/lambdas_start.py
@@ -21,7 +21,7 @@ def main():
 
     # TODO: Use lambdas to accomplish the same thing
     print(list(map(lambda t: (t-32) * 5/9,ftemps)))
-    print(list(map(lambda t: (t* 9/5) + 32,ftemps)))
+    print(list(map(lambda t: (t* 9/5) + 32,ctemps)))
 
 if __name__ == "__main__":
     main()
